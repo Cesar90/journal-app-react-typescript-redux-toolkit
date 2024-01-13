709799e1bf7b5ed29a2a6894380b7d251bcbf0d0
Cesar <ccordero.007@gmail.cim>
2024-01-13 07:04:26 -0600

    Changing of BrowserRouter to HashRouter

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -5,8 +5,8 @@ import { JournalRoutes } from "../journal/routes/JournalRoutes"
 export const AppRouter = () => {
   return (
     <Routes>
-        <Route path="/auth/*" element={<AuthRoutes />} />
-        <Route path="/*" element={<JournalRoutes />} />
+      <Route path="/*" element={<JournalRoutes />} />
+      <Route path="/auth/*" element={<AuthRoutes />} />
     </Routes>
   )
 }

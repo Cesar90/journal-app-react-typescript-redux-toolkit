4ab742fba2d992485a034f1bfda1ff91f89f1ce9
Cesar <ccordero.007@gmail.cim>
2024-01-17 06:47:58 -0600

    Show notes in sidebar

--- src/journal/components/SideBar.tsx
+++ src/journal/components/SideBar.tsx
@@ -1,9 +1,10 @@
-import { Box, Divider, Drawer, Grid, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from '@mui/material'
-import { TurnedInNot } from '@mui/icons-material';
+import { Box, Divider, Drawer, List, Toolbar, Typography } from '@mui/material'
 import { useAppSelector } from '../../store';
+import { SideBarItem } from './SideBarItem';
 
 export const SideBar = ({ drawerWidth = 240 }) => {
-    const { displayName } = useAppSelector(state => state.auth)
+    const { displayName } = useAppSelector(state => state.auth);
+    const { notes } = useAppSelector(state => state.journal)
 
     return (
         <Box
@@ -27,18 +28,8 @@ export const SideBar = ({ drawerWidth = 240 }) => {
 
                 <List>
                     {
-                        ['Enero', 'Febrero', 'Marzo', 'Abril'].map(text => (
-                            <ListItem key={text} disablePadding>
-                                <ListItemButton>
-                                    <ListItemIcon>
-                                        <TurnedInNot />
-                                    </ListItemIcon>
-                                    <Grid container>
-                                        <ListItemText primary={text} />
-                                        <ListItemText secondary={'Exercitation cillum irure elit consectetur.'} />
-                                    </Grid>
-                                </ListItemButton>
-                            </ListItem>
+                        notes.map(note => (
+                            <SideBarItem key={note.id} {...note} />
                         ))
                     }
                 </List>
---
+++ src/journal/components/SideBarItem.tsx
@@ -0,0 +1,24 @@
+import { FC, useMemo } from "react"
+import { Grid, ListItem, ListItemButton, ListItemIcon, ListItemText } from "@mui/material"
+import { TurnedInNot } from "@mui/icons-material"
+
+export const SideBarItem: FC<TNote> = ({ id, title, body }) => {
+
+    const newTitle = useMemo(() => {
+        return title.length > 17 ? title.substring(0, 17) + "..." : title
+    }, [title])
+
+    return (
+        <ListItem key={id} disablePadding>
+            <ListItemButton>
+                <ListItemIcon>
+                    <TurnedInNot />
+                </ListItemIcon>
+                <Grid container>
+                    <ListItemText primary={newTitle} />
+                    <ListItemText secondary={body} />
+                </Grid>
+            </ListItemButton>
+        </ListItem>
+    )
+}

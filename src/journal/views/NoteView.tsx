0163f867cb7b88cd911bbf52d3d1a85255d1882b
Cesar <ccordero.007@gmail.cim>
2024-01-17 07:55:30 -0600

    set active note to edit

--- src/journal/views/NoteView.tsx
+++ src/journal/views/NoteView.tsx
@@ -1,16 +1,36 @@
+import { Controller, useForm } from 'react-hook-form';
 import { SaveOutlined } from '@mui/icons-material';
 import { Button, Grid, TextField, Typography } from '@mui/material';
 import { ImageGallery } from '../components'
+import { useAppSelector } from '../../store';
+import { useMemo } from 'react';
 
 
 export const NoteView = () => {
+
+    const { active: note } = useAppSelector(state => state.journal)
+    const { control, handleSubmit } = useForm({
+        defaultValues: {
+            title: note?.title,
+            body: note?.body,
+        },
+    });
+
+    const dateString = useMemo(() => {
+        if (note?.date) {
+            const newDate = new Date(note.date);
+            return newDate.toUTCString()
+        }
+        return note?.date
+    }, [note?.date])
+
     return (
         <Grid
             className='animate__animated animate__fadeIn animate__faster'
             container
             direction='row' justifyContent='space-between' alignItems='center' sx={{ mb: 1 }}>
             <Grid item>
-                <Typography fontSize={39} fontWeight='light' >28 de agosto, 2023</Typography>
+                <Typography fontSize={39} fontWeight='light' >{dateString}</Typography>
             </Grid>
             <Grid item>
                 <Button color="primary" sx={{ padding: 2 }}>
@@ -20,22 +40,36 @@ export const NoteView = () => {
             </Grid>
 
             <Grid container>
-                <TextField
-                    type="text"
-                    variant="filled"
-                    fullWidth
-                    placeholder="Ingrese un título"
-                    label="Título"
-                    sx={{ border: 'none', mb: 1 }}
+                <Controller
+                    name="title"
+                    control={control}
+                    render={({ field }) => (
+                        <TextField
+                            {...field}
+                            type="text"
+                            variant="filled"
+                            fullWidth
+                            placeholder="Add a title"
+                            label="Title"
+                            sx={{ border: 'none', mb: 1 }}
+                        />
+                    )}
                 />
 
-                <TextField
-                    type="text"
-                    variant="filled"
-                    fullWidth
-                    multiline
-                    placeholder="¿Qué sucedió en el día de hoy?"
-                    minRows={5}
+                <Controller
+                    name="body"
+                    control={control}
+                    render={({ field }) => (
+                        <TextField
+                            {...field}
+                            type="text"
+                            variant="filled"
+                            fullWidth
+                            multiline
+                            placeholder="Body"
+                            minRows={5}
+                        />
+                    )}
                 />
             </Grid>
 

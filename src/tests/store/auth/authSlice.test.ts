5c5f8a800c10e4691733c839a1764c0be0a3febf
Cesar <ccordero.007@gmail.cim>
2024-01-18 17:44:16 -0600

    Testing authSlice rest

--- src/tests/store/auth/authSlice.test.ts
+++ src/tests/store/auth/authSlice.test.ts
@@ -1,10 +1,54 @@
-import { authSlice } from "../../../store/auth/authSlice"
-import { initialState } from "../../fixtures/authFixtures";
+import { authSlice, checkingCredentials, login, logout } from "../../../store/auth/authSlice"
+import { demoUser, initialState, initialStateAuthenticated } from "../../fixtures/authFixtures";
 
 describe("Testing in the authSlice", () => {
     test("must of return inital state and call it 'auth'", () => {
         expect(authSlice.name).toBe("auth");
         const state = authSlice.reducer(initialState, { type: "" });
         expect(state).toEqual(initialState);
+    });
+    test("Must make authentication", () => {
+        const state = authSlice.reducer(initialState, login(demoUser));
+        expect(state).toEqual({
+            status: "authenticated",
+            uid: state.uid,
+            email: state.email,
+            displayName: state.displayName,
+            photoURL: state.photoURL,
+            errorMessage: undefined,
+        })
+    });
+    test("Must make logout without args", () => {
+        const state = authSlice.reducer(initialStateAuthenticated, logout({}));
+        expect(state).toEqual(
+            {
+                status: 'not-authenticated',
+                uid: undefined,
+                email: undefined,
+                displayName: undefined,
+                photoURL: undefined,
+                errorMessage: undefined
+            }
+        )
+    });
+    test("Must make logout and show message", () => {
+        const errorMessage = "Credential are not correct";
+        const state = authSlice.reducer(initialStateAuthenticated, logout({ errorMessage }));
+        expect(state).toEqual(
+            {
+                status: 'not-authenticated',
+                uid: undefined,
+                email: undefined,
+                displayName: undefined,
+                photoURL: undefined,
+                errorMessage
+            }
+        )
+    });
+
+    test("Must change state to checking", () => {
+        const state = authSlice.reducer(initialStateAuthenticated, checkingCredentials());
+        expect(state.status).toBe('checking');
+
     });
 });
